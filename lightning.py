fee10c7ff0b290479565b2580d4ba17eed845c25
Simrun Basuita <hello@sim.run>
2018-08-02 08:59:35 +0100

    Code cleanup

--- lightning.py
+++ lightning.py
@@ -5,12 +5,13 @@ import aubio
 
 sample_rate = 44100
 
-hop_size = round(sample_rate * 1/1000) # we take a pitch measurement every hop_size frames
-window = 2048 # number of previous frames analysed for each pitch measurement
+hop_size = round(sample_rate * 1/1000)  # we take a pitch measurement every hop_size frames
+window = 2048  # number of previous frames analysed for each pitch measurement
 
 pitcher = aubio.pitch("default", window, hop_size, sample_rate)
 
 input_device = soundcard.get_microphone('Rocksmith')
+
 
 # Collapse octaves and map notes linearly between 0 and 1
 def normalise(freq):
@@ -29,6 +30,6 @@ def normalise(freq):
 with input_device.recorder(sample_rate) as recorder:
     while True:
         samples = np.array(recorder.record(hop_size), dtype=np.float32)
-        freq = pitcher(samples)[0]
+        pitch = pitcher(samples)[0]
 
-        print("%.2f" % normalise(freq),  end='\r')
+        print("%.2f" % normalise(pitch),  end='\r')
